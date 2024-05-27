b5bc679e08b38a1f1f32bff3681d2c12234e6411
Sandy Gomez <sgomez1029@gmail.com>
2024-05-27 01:12:50 -0700

    Initial Commit

---
+++ CreatingAnimationSwiftUI/CreatingAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingAnimationSwiftUIApp.swift
+//  CreatingAnimationSwiftUI
+//
+//  Created by Sandra Gomez on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CreatingAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
